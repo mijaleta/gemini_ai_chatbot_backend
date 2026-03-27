d38229bdd5965296087fe62a64c662b92f66a442
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-27 11:34:07 +0300

    updated

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_message', 'ai_reply', 'timestamp')
+    search_fields = ('user_message', 'ai_reply')
+    ordering = ('-timestamp',)
+    readonly_fields = ('timestamp',)
